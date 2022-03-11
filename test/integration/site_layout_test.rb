aa3210a2696452d6c08dae6162e190c1f1297525
Ondrej Svoboda <admin@svoby.eu>
2022-03-11 22:27:18 +0100

    layout, users controller

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,7 +6,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", home_path
-    get contact_path
-    assert_select "title", full_title("Kontakt")
   end
 end
