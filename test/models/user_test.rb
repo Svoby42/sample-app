3560552bc1efd6779106128e6f25b950e942dc27
Ondrej Svoboda <admin@svoby.eu>
2022-03-12 14:57:52 +0100

    email regex

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,6 +39,12 @@ class UserTest < ActiveSupport::TestCase
       end
   end
 
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
   # test "the truth" do
   #   assert true
   # end
