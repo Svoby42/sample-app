48dc26750ffe97030cad5880e04e17f7314dc89f
Ondrej Svoboda <admin@svoby.eu>
2022-03-15 20:20:20 +0100

    login test fix

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,7 +8,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information" do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: 'heslo' }}
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
