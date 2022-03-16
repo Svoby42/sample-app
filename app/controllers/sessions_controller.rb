eeb877c221a1ce84bdadca08e168b1113ed42d00
Ondrej Svoboda <admin@svoby.eu>
2022-03-16 11:51:01 +0100

    session remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password]) #same as user && user.authenticate
       log_in user
+      remember user
       redirect_to user #same as redirect_to user_url(user)
     else
       flash.now[:danger] = "Nesprávný email/heslo"
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
