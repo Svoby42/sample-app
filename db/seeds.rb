85478f7a2f6ff40de17b996e56a9df6e3693ec8e
Ondrej Svoboda <admin@svoby.eu>
2022-03-19 13:34:46 +0100

    paginate labels fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!(name: "Example User",
+User.create!(name: "Petr Vyskočil (admin)",
              email: "example@seznam.cz",
              password: "foobar",
              password_confirmation: "foobar",
@@ -14,7 +14,7 @@ User.create!(name: "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-User.create!(name: "Example User",
+User.create!(name: "Josef Šumfrle",
              email: "seznam@seznam.cz",
              password: "foobar",
              password_confirmation: "foobar",
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -13,14 +13,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Invalid submission
     post microposts_path, params: { micropost: { content: "" } }
     assert_select 'div#error_explanation'
-    assert_select 'a[href=?]', '/?page=2'  # Correct pagination link
+    assert_select 'a[href=?]', '/home?page=2'  # Correct pagination link
     # Valid submission
     content = "This micropost really ties the room together"
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content,
-                                                   image:   image } }
+      post microposts_path, params: { micropost: { content: content } }
     end
-    assert assigns(:micropost).image.attached?
     follow_redirect!
     assert_match content, response.body
     # Delete a post.
@@ -32,6 +30,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a different user (no delete links).
     get user_path(users(:lena))
     assert_select 'a', { text: 'delete', count: 0 }
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "#{other_user.microposts.count} micropost", response.body
   end
 
 end
