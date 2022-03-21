08ebe287e4fa3a0ef0abbb5585b07118d5321600
Ondrej Svoboda <admin@svoby.eu>
2022-03-21 21:53:33 +0100

    relationship test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user,  only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,    only: [:edit, :update]
   before_action :admin_user,      only: :destroy
 
@@ -21,11 +21,8 @@ class UsersController < ApplicationController
     if @user.save
       reset_session
       @user.send_activation_email
-      #log_in @user
-      #flash[:success] = "Vítejte!"
       flash[:info] = "Prosím zkontrolujte si svůj email, byl odeslán odkaz pro aktivaci účtu"
       redirect_to root_url
-      #redirect_to @user
     else
       render 'new'
     end
@@ -49,6 +46,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "Uživatel smazán"
     redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
